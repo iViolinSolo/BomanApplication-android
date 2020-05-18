4afd64465a462fe305444c45fa44f21a0bc1f3b7
ViolinSolo <i.violinsolo@gmail.com>
2020-05-18 14:32:04 +0800

    copy module to test app libs

--- testlibsapp/src/main/java/me/violinsolo/testlibsapp/activity/MainActivity.java
+++ testlibsapp/src/main/java/me/violinsolo/testlibsapp/activity/MainActivity.java
@@ -2,14 +2,51 @@ package me.violinsolo.testlibsapp.activity;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-import me.violinsolo.testlibsapp.R;
+import me.violinsolo.testlibsapp.base.BaseActivity;
+import me.violinsolo.testlibsapp.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity<ActivityMainBinding> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * need to initilize the ViewBinding Class in every sub activity class.
+     *
+     * @return mBinder the field of view binding handler. Initialize it before use
+     * eg.
+     * mBinder = ActivityXMLNameBinding.inflate(getLayoutInflater());
+     */
+    @Override
+    protected ActivityMainBinding onBind() {
+        return ActivityMainBinding.inflate(getLayoutInflater());
+    }
+
+    /**
+     * init data here, like you can get data from extra.
+     * eg.
+     * var data = getIntent().getParcelableExtra(EXTRA_KEY_XXX);
+     */
+    @Override
+    protected void initData() {
+
+    }
+
+    /**
+     * write init view codes, such as toolbar.
+     */
+    @Override
+    protected void initViews() {
+
+    }
+
+    /**
+     * bind all listeners here.
+     */
+    @Override
+    protected void bindListeners() {
+
     }
 }
