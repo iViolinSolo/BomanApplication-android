0847115de58a1b5b5854a95054fab52cc43fa93b
ViolinSolo <i.violinsolo@gmail.com>
2020-05-18 10:35:39 +0800

    add module test all such libs

---
+++ testlibsapp/src/main/java/me/violinsolo/testlibsapp/base/BaseActivity.java
@@ -0,0 +1,51 @@
+package me.violinsolo.testlibsapp.base;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewbinding.ViewBinding;
+
+public abstract class BaseActivity<T extends ViewBinding > extends AppCompatActivity {
+
+    protected T mBinder;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBinder = onBind();
+
+        setContentView(mBinder.getRoot());
+
+        initData();
+        initViews();
+        bindListeners();
+    }
+
+    /**
+     * need to initilize the ViewBinding Class in every sub activity class.
+     *
+     * @return mBinder the field of view binding handler. Initialize it before use
+     *                  eg.
+     *                  mBinder = ActivityXMLNameBinding.inflate(getLayoutInflater());
+     */
+    protected abstract T onBind();
+
+    /**
+     * init data here, like you can get data from extra.
+     *  eg.
+     *  var data = getIntent().getParcelableExtra(EXTRA_KEY_XXX);
+     */
+    protected abstract void initData();
+
+    /**
+     * write init view codes, such as toolbar.
+     */
+    protected abstract void initViews();
+
+    /**
+     * bind all listeners here.
+     */
+    protected abstract void bindListeners();
+}
---
+++ testlibsapp/src/main/java/me/violinsolo/testlibsapp/base/BaseFragment.java
@@ -0,0 +1,49 @@
+package me.violinsolo.testlibsapp.base;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewbinding.ViewBinding;
+
+public abstract class BaseFragment<T extends ViewBinding> extends Fragment {
+
+    protected T mBinder;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        return super.onCreateView(inflater, container, savedInstanceState);
+
+        mBinder = onBind(inflater, container);
+
+        bindListeners();
+
+        return mBinder.getRoot();
+    }
+
+
+    /**
+     * need to initilize the ViewBinding Class in every sub activity class.
+     *
+     * @return mBinder the field of view binding handler. Initialize it before use
+     *                  eg.
+     *                  mBinder = FragmentXMLNameBinding.inflate(inflater, container, boolean attachToParent = false);
+     */
+    protected abstract T onBind(@NonNull LayoutInflater inflater, @Nullable ViewGroup parent);
+
+
+    /**
+     * bind all listeners here.
+     */
+    protected abstract void bindListeners();
+}
