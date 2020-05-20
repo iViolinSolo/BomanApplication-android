48c02827ebef2365eede18b6bd535888afc22a5c
ViolinSolo <i.violinsolo@gmail.com>
2020-05-20 18:47:05 +0800

    add location check ui

--- app/src/main/java/me/violinsolo/boman/activity/prerequisite/LocationCheckActivity.java
+++ app/src/main/java/me/violinsolo/boman/activity/prerequisite/LocationCheckActivity.java
@@ -1,15 +1,24 @@
 package me.violinsolo.boman.activity.prerequisite;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import me.violinsolo.boman.R;
 import me.violinsolo.boman.base.BaseActivity;
 import me.violinsolo.boman.databinding.ActivityLocationCheckBinding;
 
 public class LocationCheckActivity extends BaseActivity<ActivityLocationCheckBinding> {
+    public Context mContext;
+    private static final int REQUEST_CODE_OPEN_GPS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +45,7 @@ public class LocationCheckActivity extends BaseActivity<ActivityLocationCheckBin
      */
     @Override
     protected void initData() {
+        mContext = LocationCheckActivity.this;
 
     }
 
@@ -46,17 +56,42 @@ public class LocationCheckActivity extends BaseActivity<ActivityLocationCheckBin
     protected void initViews() {
 
         setSupportActionBar(mBinder.toolbar);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//添加默认的返回图标
-//        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//添加默认的返回图标
+        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
 
-        if (checkGPSIsOpen()) {
-            Intent intent = new Intent(LocationCheckActivity.this, BluetoothCheckActivity.class);
-
-            startActivity(intent);
-            finish();
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || checkGPSIsOpen()) {
+            goToNextPage();
         }else {
 
+            mBinder.btnOk.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    new AlertDialog.Builder(mContext)
+                            .setTitle(R.string.notifyTitle)
+                            .setMessage(R.string.gpsNotifyMsg)
+                            .setNegativeButton(R.string.cancel,
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            finish();
+                                        }
+                                    })
+                            .setPositiveButton(R.string.setting,
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                            startActivityForResult(intent, REQUEST_CODE_OPEN_GPS);
+                                        }
+                                    })
+
+                            .setCancelable(false)
+                            .show();
+                }
+            });
         }
+
+
     }
 
     /**
@@ -65,6 +100,26 @@ public class LocationCheckActivity extends BaseActivity<ActivityLocationCheckBin
     @Override
     protected void bindListeners() {
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_OPEN_GPS) {
+            if (checkGPSIsOpen()) {
+                goToNextPage();
+            }else {
+                Toast.makeText(mContext, "请授予我们位置权限", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void goToNextPage() {
+
+        Intent intent = new Intent(mContext, BluetoothCheckActivity.class);
+
+        startActivity(intent);
+        finish();
     }
 
     private boolean checkGPSIsOpen() {
