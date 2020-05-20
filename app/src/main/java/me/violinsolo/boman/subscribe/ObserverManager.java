4b51296d8dcc007dbef629cde5abbed6fe126ef1
ViolinSolo <i.violinsolo@gmail.com>
2020-05-20 15:33:19 +0800

    add disconnect observers.

--- app/src/main/java/me/violinsolo/boman/subscribe/ObserverManager.java
+++ app/src/main/java/me/violinsolo/boman/subscribe/ObserverManager.java
@@ -27,12 +27,12 @@ public class ObserverManager implements Observable {
     private List<Observer> observers = new ArrayList<>();
 
     @Override
-    public void addObserver(Observer obj) {
+    public synchronized void addObserver(Observer obj) {
         observers.add(obj);
     }
 
     @Override
-    public void deleteObserver(Observer obj) {
+    public synchronized void deleteObserver(Observer obj) {
         int i = observers.indexOf(obj);
         if (i >= 0) {
             observers.remove(obj);
@@ -40,7 +40,7 @@ public class ObserverManager implements Observable {
     }
 
     @Override
-    public void notifyObserver(BleDevice bleDevice) {
+    public synchronized void notifyObserver(BleDevice bleDevice) {
         for (int i = 0; i < observers.size(); i++) {
             Observer o = observers.get(i);
             o.disConnected(bleDevice);
