f9276465086bdc125c130c96fc4bd4b22fb796c0
ViolinSolo <i.violinsolo@gmail.com>
2020-05-20 17:01:11 +0800

    change the inheritance tree

--- app/src/main/java/me/violinsolo/boman/activity/prerequisite/LocationCheckActivity.java
+++ app/src/main/java/me/violinsolo/boman/activity/prerequisite/LocationCheckActivity.java
@@ -2,14 +2,53 @@ package me.violinsolo.boman.activity.prerequisite;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
 import me.violinsolo.boman.R;
+import me.violinsolo.boman.base.BaseActivity;
+import me.violinsolo.boman.databinding.ActivityLocationCheckBinding;
 
-public class LocationCheckActivity extends AppCompatActivity {
+public class LocationCheckActivity extends BaseActivity<ActivityLocationCheckBinding> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_check);
+    }
+
+    /**
+     * need to initilize the ViewBinding Class in every sub activity class.
+     *
+     * @return mBinder the field of view binding handler. Initialize it before use
+     * eg.
+     * mBinder = ActivityXMLNameBinding.inflate(getLayoutInflater());
+     */
+    @Override
+    protected ActivityLocationCheckBinding onBind() {
+        return ActivityLocationCheckBinding.inflate(getLayoutInflater());
+    }
+
+    /**
+     * init data here, like you can get data from extra.
+     * eg.
+     * var data = getIntent().getParcelableExtra(EXTRA_KEY_XXX);
+     */
+    @Override
+    protected void initData() {
+
+    }
+
+    /**
+     * write init view codes, such as toolbar.
+     */
+    @Override
+    protected void initViews() {
+
+    }
+
+    /**
+     * bind all listeners here.
+     */
+    @Override
+    protected void bindListeners() {
+
     }
 }
