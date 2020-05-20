bf1a271acfe848bf10971606e7492c09ec765665
ViolinSolo <i.violinsolo@gmail.com>
2020-05-20 15:17:05 +0800

    add observers.

---
+++ app/src/main/java/me/violinsolo/boman/subscribe/Observable.java
@@ -0,0 +1,21 @@
+package me.violinsolo.boman.subscribe;
+
+
+import com.clj.fastble.data.BleDevice;
+
+/**
+ * @author violinsolo
+ * @version Boman v0.1
+ * @createAt 2020/5/20 10:18 AM
+ * @updateAt 2020/5/20 10:18 AM
+ * <p>
+ * Copyright (c) 2020 EmberXu.hack. All rights reserved.
+ */
+public interface Observable {
+
+    void addObserver(me.violinsolo.boman.ble.Observer obj);
+
+    void deleteObserver(Observer obj);
+
+    void notifyObserver(BleDevice bleDevice);
+}
--- app/src/main/java/me/violinsolo/boman/subscribe/ObserverManager.java
+++ app/src/main/java/me/violinsolo/boman/subscribe/ObserverManager.java
@@ -1,5 +1,11 @@
 package me.violinsolo.boman.subscribe;
 
+
+import com.clj.fastble.data.BleDevice;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author violinsolo
  * @version Boman v0.1
@@ -8,5 +14,37 @@ package me.violinsolo.boman.subscribe;
  * <p>
  * Copyright (c) 2020 EmberXu.hack. All rights reserved.
  */
-public class ObserverManager {
+public class ObserverManager implements Observable {
+
+    public static ObserverManager getInstance() {
+        return ObserverManagerHolder.sObserverManager;
+    }
+
+    private static class ObserverManagerHolder {
+        private static final ObserverManager sObserverManager = new ObserverManager();
+    }
+
+    private List<Observer> observers = new ArrayList<>();
+
+    @Override
+    public void addObserver(Observer obj) {
+        observers.add(obj);
+    }
+
+    @Override
+    public void deleteObserver(Observer obj) {
+        int i = observers.indexOf(obj);
+        if (i >= 0) {
+            observers.remove(obj);
+        }
+    }
+
+    @Override
+    public void notifyObserver(BleDevice bleDevice) {
+        for (int i = 0; i < observers.size(); i++) {
+            Observer o = observers.get(i);
+            o.disConnected(bleDevice);
+        }
+    }
+
 }
