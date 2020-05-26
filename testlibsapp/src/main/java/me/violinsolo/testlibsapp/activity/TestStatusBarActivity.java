c1f444b84799e394fbaf86b855ed444ec8ae5251
ViolinSolo <i.violinsolo@gmail.com>
2020-05-26 10:09:29 +0800

    add test statusbar util

---
+++ testlibsapp/src/main/java/me/violinsolo/testlibsapp/activity/TestStatusBarActivity.java
@@ -0,0 +1,15 @@
+package me.violinsolo.testlibsapp.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import me.violinsolo.testlibsapp.R;
+
+public class TestStatusBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_status_bar);
+    }
+}
