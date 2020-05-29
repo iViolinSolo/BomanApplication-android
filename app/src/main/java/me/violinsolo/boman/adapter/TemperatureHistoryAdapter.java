d409900bbe0fc753eb7375b70557f0116638c6f2
ViolinSolo <i.violinsolo@gmail.com>
2020-05-29 18:01:38 +0800

    add adapter...

---
+++ app/src/main/java/me/violinsolo/boman/adapter/TemperatureHistoryAdapter.java
@@ -0,0 +1,19 @@
+package me.violinsolo.boman.adapter;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * @author violinsolo
+ * @version Boman v0.1
+ * @createAt 2020/5/29 3:30 PM
+ * @updateAt 2020/5/29 3:30 PM
+ * <p>
+ * Copyright (c) 2020 EmberXu.hack. All rights reserved.
+ */
+public class TemperatureHistoryAdapter {
+
+
+    public static class TemperatureRecordViewHolder extends RecyclerView.ViewHolder {
+
+    }
+}
